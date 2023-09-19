15a75cf64d3706581c91b5a4b89b61c0378ee68e
ochegbinu <ochegbinuisaac@gmail.com>
2023-09-19 23:13:37 +0100

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Seo from "./components/services/Seo";
 import Smm from "./components/services/Smm";
 import Em from "./components/services/Em";
 import Sem from "./components/services/Sem";
+import Branding from "./components/services/Branding";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
             <Route path='/social-media-marketing' element={<Smm />} />
             <Route path='/email-marketing' element={<Em />} />
             <Route path='/search-engine-marketing' element={<Sem />} />
+            <Route path='/branding' element={<Branding />} />
           </Routes>
         </div>
       </Router>
---
+++ src/components/services/Branding.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import TopNavbar from '../layout/TopNavbar';
+
+export default function Branding() {
+  return (
+    <>
+<TopNavbar />
+
+
+    </>
+  )
+}
