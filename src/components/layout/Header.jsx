3937f2af08d7a01547dc12c54a90eac5df500ffc
ochegbinu <ochegbinuisaac@gmail.com>
2023-09-21 22:04:30 +0100

    update

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -8,7 +8,7 @@ export default function
         <div className="header">
               <div className="content">
                   <h1>
-                    We're your Partner for Digital Marketing Success
+                    We're your Partner for Digital Marketing <br /> Success
                   </h1>
                   <p className='rapp' >Empower Your Brand, and Drive Results with AmorServ Consulting</p>
                   <a href=""><button>Get Expert</button></a>
