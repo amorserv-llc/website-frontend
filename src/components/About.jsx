3d2e15036b60bdb7a2990a9db0a557aa8adede04
ochegbinu <ochegbinuisaac@gmail.com>
2023-10-12 00:07:37 +0100

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -37,7 +37,7 @@ export default function About() {
         <Navbar />
       </div>
 
-      <header className='header-bg' style={{ height: "35.5rem" }}>
+      <header className='header-bg' style={{ height: "35.7rem" }}>
         <div className='header'>
           <div className='content'>
             <h1>Curious to Know Our Story?</h1>
