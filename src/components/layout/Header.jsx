44630cffea113c995c68e040f53e9164219e4503
ochegbinu <ochegbinuisaac@gmail.com>
2023-10-26 23:45:58 +0100

    update

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,10 +2,8 @@
 
 import React from "react";
 import homeimg from "../../Assets/homeimg.png";
-import { Link } from "react-router-dom";
 import HeroBg from "../organism/hero-bg";
 export default function () {
-
   const handleCallButtonClick = () => {
     const contactFormSection = document.getElementById("contact-form-section");
     if (contactFormSection) {
@@ -14,15 +12,11 @@ export default function () {
   };
   return (
     <HeroBg
-    title={"We're your Partner for Digital Marketing Success".toUpperCase()}
-    desc="Empower Your Brand, and Drive Results with AmorServ Consulting"
-    onClick={handleCallButtonClick} // Pass the function as onClick prop
-
-    link=""
-    
-    linkTitle="Start Today"
-    img={homeimg}
-  />
-
+      title={"We're your Partner for Digital Marketing Success".toUpperCase()}
+      desc='Empower Your Brand, and Drive Results with AmorServ Consulting'
+      link=''
+      linkTitle='Start Today'
+      img={homeimg}
+    />
   );
 }
