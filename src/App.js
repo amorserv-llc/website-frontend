dcd777800eab8773307694ec1d9059ddbb077ce3
ochegbinu <ochegbinuisaac@gmail.com>
2023-11-04 11:07:19 +0100

    God Abeg make e work

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+/** @format */
 
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -13,26 +13,108 @@ import Em from "./components/pages/services/Em";
 import Seo from "./components/pages/Seo";
 import Sem from "./components/pages/services/Sem";
 import TermsCondition from "./components/pages/TermsCondition";
-import ScrollToTop from "../src/components/layouts/ScrollToTop"
+import ScrollToTop from "../src/components/layouts/ScrollToTop";
 import ThankYou from "./components/pages/ThankYou";
 import Branding from "./components/pages/services/Branding";
+import ErrorPage from "./components/pages/errorpage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/blog' element={<ScrollToTop> <Blog /> </ScrollToTop >} />
-        <Route path='/contact-us' element={<ScrollToTop> <Contact /> </ScrollToTop >} />
-        <Route path='/about-us' element={<ScrollToTop> <About /> </ScrollToTop >} />
-        <Route path='/portfolio' element={<ScrollToTop> <Portfolio /> </ScrollToTop >} />
-        <Route path='/terms-conditions' element={<TermsCondition />} />
-        <Route path='/search-engine-optimization' element={<ScrollToTop> <Seo /> </ScrollToTop >} />
-        <Route path='/social-media-marketing' element={<ScrollToTop> <Smm /> </ScrollToTop >} />
-        <Route path='/email-marketing' element={<ScrollToTop> <Em /> </ScrollToTop >} />
-        <Route path='/search-engine-marketing' element={<ScrollToTop> <Sem /> </ScrollToTop >} />
-        <Route path='/thank-you' element={<ScrollToTop> <ThankYou /> </ScrollToTop >} />
-        <Route path='/branding' element={<ScrollToTop><Branding /></ScrollToTop>} />
+        <Route path='/' errorElement={<ErrorPage />}>
+          <Route index element={<Home />} />
+          <Route
+            path='/blog'
+            element={
+              <ScrollToTop>
+                {" "}
+                <Blog />{" "}
+              </ScrollToTop>
+            }
+          />
+          <Route
+            path='/contact-us'
+            element={
+              <ScrollToTop>
+                {" "}
+                <Contact />{" "}
+              </ScrollToTop>
+            }
+          />
+          <Route
+            path='/about-us'
+            element={
+              <ScrollToTop>
+                {" "}
+                <About />{" "}
+              </ScrollToTop>
+            }
+          />
+          <Route
+            path='/portfolio'
+            element={
+              <ScrollToTop>
+                {" "}
+                <Portfolio />{" "}
+              </ScrollToTop>
+            }
+          />
+          <Route path='/terms-conditions' element={<TermsCondition />} />
+          <Route
+            path='/search-engine-optimization'
+            element={
+              <ScrollToTop>
+                {" "}
+                <Seo />{" "}
+              </ScrollToTop>
+            }
+          />
+          <Route
+            path='/social-media-marketing'
+            element={
+              <ScrollToTop>
+                {" "}
+                <Smm />{" "}
+              </ScrollToTop>
+            }
+          />
+          <Route
+            path='/email-marketing'
+            element={
+              <ScrollToTop>
+                {" "}
+                <Em />{" "}
+              </ScrollToTop>
+            }
+          />
+          <Route
+            path='/search-engine-marketing'
+            element={
+              <ScrollToTop>
+                {" "}
+                <Sem />{" "}
+              </ScrollToTop>
+            }
+          />
+          <Route
+            path='/thank-you'
+            element={
+              <ScrollToTop>
+                {" "}
+                <ThankYou />{" "}
+              </ScrollToTop>
+            }
+          />
+          <Route
+            path='/branding'
+            element={
+              <ScrollToTop>
+                <Branding />
+              </ScrollToTop>
+            }
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
