aed3cfde647e43660494d31dccf9078bd0f228a6
ochegbinu <ochegbinuisaac@gmail.com>
2023-11-06 19:56:13 +0100

    making crusial

--- src/index.js
+++ src/index.js
@@ -1,26 +1,62 @@
+
+import React from "react"
 import './index.css';
 import './styles/about.css';
 import './styles/faq.css';
 import './styles/service.css';
 import './styles/trust.css';
 import './styles/contact.css';
-import React from "react";
+import HttpsRedirect from "react-https-redirect";
+import App from "./App.js";
+import { render } from 'react-dom';
 import { createRoot } from "react-dom/client";
-import { render } from "react-dom";
-import "./index.css";
-import App from "./App";
-import HttpsRedirect from 'react-https-redirect';
+
+ 
 const APP = (
- <React.StrictMode>
-<HttpsRedirect>
-<App />
-</HttpsRedirect>
- </React.StrictMode>
+  <React.StrictMode>
+        <HttpsRedirect>
+          <App />
+        </HttpsRedirect>
+  </React.StrictMode>
 );
+ 
+ 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+ 
 if (rootElement.hasChildNodes()) {
- render(APP, rootElement);
-} else {
- root.render(APP);
-}
+  render(APP, rootElement);
+ } else {
+  root.render(APP);
+ }
+ 
+//  
+
+
+
+// import './index.css';
+// import './styles/about.css';
+// import './styles/faq.css';
+// import './styles/service.css';
+// import './styles/trust.css';
+// import './styles/contact.css';
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import { render } from "react-dom";
+// import "./index.css";
+// import App from "./App";
+// import HttpsRedirect from 'react-https-redirect';
+// const APP = (
+//  <React.StrictMode>
+// <HttpsRedirect>
+// <App />
+// </HttpsRedirect>
+//  </React.StrictMode>
+// );
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+// if (rootElement.hasChildNodes()) {
+//  render(APP, rootElement);
+// } else {
+//  root.render(APP);
+// }
