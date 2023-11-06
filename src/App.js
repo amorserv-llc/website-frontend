ae4f18ae14bff218b40da0e993ccbfacd20f54e2
ochegbinu <ochegbinuisaac@gmail.com>
2023-11-06 21:57:35 +0100

    main

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const Home = lazy(() => import("./Pages/Home/Home"));
-// const About = lazy(() => import("./Pages/About/About"));
-// const Portfolio = lazy(() => import("./Pages/Portfolio/Portfolio"));
+const About = lazy(() => import("./Pages/About/About"));
+const Portfolio = lazy(() => import("./Pages/Portfolio/Portfolio"));
 const Blog = lazy(() => import("./Pages/Blog/Blog"));
-// const Contact = lazy(() => import("./Pages/Contact/Contact"));
-// const TermsCondition = lazy(() => import("./Pages/TermsCondition/TermsCondition"));
+const Contact = lazy(() => import("./Pages/Contact/Contact"));
+const TermsCondition = lazy(() => import("./Pages/TermsCondition/TermsCondition"));
 const Seo = lazy(() => import("./Pages/Seo/Seo"));
 const Smm = lazy(() => import("./Pages/Smm/Smm"));
 const Em = lazy(() => import("./Pages/Em/Em"));
@@ -31,14 +31,14 @@ const App = () => {
           <Route path='/' errorElement={<ErrorPage />} element={<Layout />}>
             {" "}
             <Route index element={<Home />} />{" "}
-            {/* <Route path='/about-us' element={<About />} />{" "} */}
-            {/* <Route path='/contact-us' element={<Contact />} />{" "} */}
-            {/* <Route path='/portfolio-page' element={<Portfolio />} />{" "} */}
+            <Route path='/about-us' element={<About />} />{" "}
+            <Route path='/contact-us' element={<Contact />} />{" "}
+            <Route path='/portfolio-page' element={<Portfolio />} />{" "}
             <Route path='/search-engine-optimization' element={<Seo />} />{" "}
             <Route path='/social-media-marketing' element={<Smm />} />{" "}
             <Route path='/search-engine-marketing' element={<Sem />} />{" "}
             <Route path='/email-marketing' element={<Em />} />{" "}
-            {/* <Route path='/terms-conditions' element={<TermsCondition />} />{" "} */}
+            <Route path='/terms-conditions' element={<TermsCondition />} />{" "}
             <Route path='/thank-you' element={<ThankYou />} />{" "}
             <Route path='/blog' element={<Blog />} />{" "}
             <Route path='/branding' element={<Branding />} />{" "}
