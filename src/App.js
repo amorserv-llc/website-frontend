e1ffaa929fbcec3c48d93a6f7cd697c8910a1b73
ochegbinu <ochegbinuisaac@gmail.com>
2023-11-06 21:27:23 +0100

    .

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const Home = lazy(() => import("./Pages/Home/Home"));
-const About = lazy(() => import("./Pages/About/About"));
-const Portfolio = lazy(() => import("./Pages/Portfolio/Portfolio"));
+// const About = lazy(() => import("./Pages/About/About"));
+// const Portfolio = lazy(() => import("./Pages/Portfolio/Portfolio"));
 const Blog = lazy(() => import("./Pages/Blog/Blog"));
-const Contact = lazy(() => import("./Pages/Contact/Contact"));
-const TermsCondition = lazy(() => import("./Pages/TermsCondition/TermsCondition"));
+// const Contact = lazy(() => import("./Pages/Contact/Contact"));
+// const TermsCondition = lazy(() => import("./Pages/TermsCondition/TermsCondition"));
 const Seo = lazy(() => import("./Pages/Seo/Seo"));
 const Smm = lazy(() => import("./Pages/Smm/Smm"));
 const Em = lazy(() => import("./Pages/Em/Em"));
 const Sem = lazy(() => import("./Pages/Sem/Sem"));
 const Branding = lazy(() => import("./Pages/Branding/Branding"));
 const ThankYou = lazy(() => import("./Pages/Thanks/ThankYou"));
-const Layout = lazy(() => import("./components/layouts/layout"))
+const Layout = lazy(() => import("./components/layouts/layout"));
+const DemoPage = lazy(() => import("./Pages/DemoPage"));
+
+
 const ErrorPage = lazy(() => import("./Pages/errorpage"));
+
 
 const App = () => {
   return (
@@ -27,17 +31,18 @@ const App = () => {
           <Route path='/' errorElement={<ErrorPage />} element={<Layout />}>
             {" "}
             <Route index element={<Home />} />{" "}
-            <Route path='/about-us' element={<About />} />{" "}
-            <Route path='/contact-us' element={<Contact />} />{" "}
-            <Route path='/portfolio-page' element={<Portfolio />} />{" "}
+            {/* <Route path='/about-us' element={<About />} />{" "} */}
+            {/* <Route path='/contact-us' element={<Contact />} />{" "} */}
+            {/* <Route path='/portfolio-page' element={<Portfolio />} />{" "} */}
             <Route path='/search-engine-optimization' element={<Seo />} />{" "}
             <Route path='/social-media-marketing' element={<Smm />} />{" "}
             <Route path='/search-engine-marketing' element={<Sem />} />{" "}
             <Route path='/email-marketing' element={<Em />} />{" "}
-            <Route path='/terms-conditions' element={<TermsCondition />} />{" "}
+            {/* <Route path='/terms-conditions' element={<TermsCondition />} />{" "} */}
             <Route path='/thank-you' element={<ThankYou />} />{" "}
             <Route path='/blog' element={<Blog />} />{" "}
             <Route path='/branding' element={<Branding />} />{" "}
+            <Route path='/demo' element={<DemoPage />} />{" "}
           </Route>{" "}
         </Routes>{" "}
       </Suspense>{" "}
